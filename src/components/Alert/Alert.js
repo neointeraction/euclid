14ea4324f4d4ce3d5988299256e974cc65d292fb
cowusuamponsah <cliffowusu2727@gmail.com>
2022-06-13 08:19:02 +0000

    Added alert boxes

---
+++ src/components/Alert/Alert.js
@@ -0,0 +1,44 @@
+import CloseIcon from '@mui/icons-material/Close';
+import PropTypes from 'prop-types';
+
+import { AlertContainer } from './alert.styles';
+
+import AlertErrorIcon from 'assets/images/icons/alert-error.svg';
+import AlertWarningIcon from 'assets/images/icons/alert-warning.svg';
+import AlertSuccessIcon from 'assets/images/icons/alert-success.svg';
+
+const CusAlert = ({ type, onClose, message }) => {
+  return (
+    <AlertContainer type={type}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          gap: '6px',
+        }}
+      >
+        <img
+          src={
+            type === 'success'
+              ? AlertSuccessIcon
+              : type === 'warning'
+              ? AlertWarningIcon
+              : AlertErrorIcon
+          }
+          alt=""
+        />
+        <p>{message}</p>
+      </div>
+      <CloseIcon onClick={onClose} fontSize="small" />
+    </AlertContainer>
+  );
+};
+
+CusAlert.propTypes = {
+  type: PropTypes.string.isRequired,
+  onClose: PropTypes.func,
+  message: PropTypes.string.isRequired,
+};
+
+export default CusAlert;
